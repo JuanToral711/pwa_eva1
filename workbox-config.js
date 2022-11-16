0d3a8c38ab462bc7ade37800b5bab92a995b9ae0
Juan Toral <96216586+JuanToral711@users.noreply.github.com>
2022-11-15 20:31:52 -0600

    Add files via upload

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '.',
+	globPatterns: [
+		'**/*.{html,css,jpg,png,js,php,txt,json,scss}'
+	],
+	swDest: 'C:/xampp/htdocs/blogpersonal/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
